02e654aeb1beb95e4c045ab1d90afe13d8de4131
Aleksei Ryzhenkov <ryzhenkovaf@gmail.com>
2023-03-01 13:27:05 +0700

    add 040

---
+++ Example040/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины. 
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+Console.Write("Введите первое число:  ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число:  ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число:  ");
+int c = int.Parse(Console.ReadLine());
+
+bool Treugolnik (int a, int b, int c)
+{
+    return (((a + b) > c) && ((a + c) > b) && ((b + c) > a));
+}
+if (Treugolnik(a, b, c))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
